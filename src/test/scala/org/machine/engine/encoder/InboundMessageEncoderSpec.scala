d438f8ac6bc86c1cd0b0edefd83f09784267f982
Samuel Holloway <sholloway@gmail.com>
2016-04-05 20:12:36 -0500

    Added encoding Elements.

--- src/test/scala/org/machine/engine/encoder/InboundMessageEncoderSpec.scala
+++ src/test/scala/org/machine/engine/encoder/InboundMessageEncoderSpec.scala
@@ -9,6 +9,7 @@ import org.machine.inbound.InboundMessageEnvelope.Message.ActionType
 import org.machine.inbound.InboundMessageEnvelope.Message.ScopeType
 import org.machine.inbound.InboundMessageEnvelope.Message.EntityType
 import org.machine.inbound.InboundMessageEnvelope.Message.PropertyDefinition
+import org.machine.inbound.InboundMessageEnvelope.Message.Field
 
 //https://developers.google.com/protocol-buffers/docs/javatutorial
 //https://github.com/google/protobuf/blob/master/examples/AddPerson.java
@@ -59,13 +60,53 @@ class InboundMessageEncoderSpec extends FunSpec with Matchers with EasyMockSugar
       decodedMsg.getUser().getId should equal("abc")
 
       decodedMsg.hasDefinition() should equal(true)
+      decodedMsg.hasElement() should equal(false)
       decodedMsg.getDefinition().getId should equal("edId")
       decodedMsg.getDefinition().getName should equal("edName")
       decodedMsg.getDefinition().getDescription should equal("edDescription")
       decodedMsg.getDefinition().getPropertiesList() should have length 2
-
     }
 
-    it("should encode and decode an Element message")(pending)
+    it("should encode and decode an Element message"){
+      val schemaVersion = 2112
+      val messageBuilder = Message.newBuilder()
+      messageBuilder.setSchemaVersion(schemaVersion)
+        .setAction(ActionType.CREATE)
+        .setScope(ScopeType.SCOPE_DATA_SET)
+        .setEntityType(EntityType.ELEMENT)
+
+      messageBuilder.getUserBuilder().setId("Stevie")
+      messageBuilder.getElementBuilder().setId("e1")
+        .setElementType("edName")
+        .setDescription("an element")
+
+      val f1 = Field.newBuilder()
+        .setName("f1")
+        .setFieldType("int")
+        .setValue("123456")
+        .build()
+
+      messageBuilder.getElementBuilder().addFields(f1)
+      val bytes = messageBuilder.build().toByteArray()
+
+      val decodedMsg = Message.newBuilder()
+        .mergeFrom(bytes)
+
+      decodedMsg.getSchemaVersion should equal(schemaVersion)
+      decodedMsg.getAction should equal(ActionType.CREATE)
+      decodedMsg.getScope should equal(ScopeType.SCOPE_DATA_SET)
+      decodedMsg.getEntityType should equal(EntityType.ELEMENT)
+      decodedMsg.getUser().getId should equal("Stevie")
+
+      decodedMsg.hasElement() should equal(true)
+      decodedMsg.hasDefinition() should equal(false)
+      decodedMsg.getElement().getId should equal("e1")
+      decodedMsg.getElement().getElementType should equal("edName")
+      decodedMsg.getElement().getDescription should equal("an element")
+
+      decodedMsg.getElement().getFieldsList() should have length 1
+    }
+
+    it("should encode associations")(pending)
   }
 }
