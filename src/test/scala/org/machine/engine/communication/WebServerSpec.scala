4741667c790241ac58937541e66526f5c6c471e5
Samuel Holloway <sholloway@gmail.com>
2016-06-10 16:36:14 -0500

    Refactored basic websocket test.

--- src/test/scala/org/machine/engine/communication/WebServerSpec.scala
+++ src/test/scala/org/machine/engine/communication/WebServerSpec.scala
@@ -51,6 +51,22 @@ class WebServerSpec extends TestKit(ActorSystem("AkkaHTTPSpike")) with ImplicitS
   val engineVersion = config.getString("engine.version")
   val echoPath = s"ws://$host:$port/ws/ping"
 
+  val failTest:PartialFunction[Throwable, Any] = {
+    case e => {
+      println(e)
+      fail()
+    }
+  }
+
+  def verifyProtocolsSwitched(upgrade: WebSocketUpgradeResponse): Done = {
+    if (upgrade.response.status == StatusCodes.SwitchingProtocols) {
+      Done
+    } else {
+      throw new RuntimeException(s"Connection failed: ${upgrade.response.status}")
+    }
+  }
+
+
   /*
   TODO Test the WebServer
   1. Get an instance of the engine.
@@ -65,7 +81,6 @@ class WebServerSpec extends TestKit(ActorSystem("AkkaHTTPSpike")) with ImplicitS
   }
 
   override def afterAll(){
-    println("started after all")
     server.stop()
     TestKit.shutdownActorSystem(system)
     Engine.shutdown
@@ -75,87 +90,88 @@ class WebServerSpec extends TestKit(ActorSystem("AkkaHTTPSpike")) with ImplicitS
   describe("Receiving Requests"){
     describe("WebSocket Requests"){
       it ("should echo commands for /ping"){
-        val printSink: Sink[Message, Future[Done]] = createPrintSink()
-        val helloSource: Source[Message, NotUsed] = Source.single(TextMessage("hello world!"))
-
-        // the Future[Done] is the materialized value of Sink.foreach
-        // and it is completed when the stream completes
-        val flow: Flow[Message, Message, Future[Done]] = Flow.fromSinkAndSourceMat(printSink, helloSource)(Keep.left)
-
-        // upgradeResponse is a Future[WebSocketUpgradeResponse] that
-        // completes or fails when the connection succeeds or fails
-        // and closed is a Future[Done] representing the stream completion from above
+        val helloSource = Source.fromIterator(() => Seq(tm("A"), tm("B"), tm("C"), tm("D"), tm("E"), tm("F")).toIterator)
+        val flow = Flow.fromSinkAndSourceMat(Sink.seq[Message], helloSource)(Keep.left)
         val (upgradeResponse, closed) = Http().singleWebSocketRequest(WebSocketRequest(echoPath, subprotocol = Some("engine.json.v1")), flow)
-
-        val connected = upgradeResponse.map { upgrade =>
-          // just like a regular http request we can get 404 NotFound,
-          // with a response body, that will be available from upgrade.response
-          println("Upgrade Response Recieved")
-          if (upgrade.response.status == StatusCodes.OK) {
-            println("200 Received")
-            Done
-          } else if(upgrade.response.status == StatusCodes.SwitchingProtocols){
-            println("101 Recieved")
-          }else {
-            throw new RuntimeException(s"Connection failed: ${upgrade.response.status}")
-          }
-        }
-
-        // in a real application you would not side effect here
-        // and handle errors more carefully
-        connected.onComplete(println)
-        connected.onFailure{
-          case e => {
-            println(e)
-            fail()
-          }
-        }
-        closed.foreach(_ => println("closed"))
-
-        whenReady(connected){ response =>
-          println("stupid...")
-          println(response)
+        val connected = upgradeResponse.map(verifyProtocolsSwitched)
+        connected.onFailure(failTest)
+        whenReady(closed){ results =>
+          results should equal(Vector(TextMessage.Strict("A"),
+            TextMessage.Strict("B"),
+            TextMessage.Strict("C"),
+            TextMessage.Strict("D"),
+            TextMessage.Strict("E"),
+            TextMessage.Strict("F")))
         }
       }
 
-      it ("should AssociateElements")(pending)
-      it ("should CommandFactory")(pending)
-      it ("should CommandScope")(pending)
-      it ("should CreateDataSet")(pending)
-      it ("should CreateElement")(pending)
-      it ("should CreateElementDefinition")(pending)
-      it ("should DeleteAssociation")(pending)
-      it ("should DeleteElement")(pending)
-      it ("should DeleteElementDefintion")(pending)
-      it ("should EditAssociation")(pending)
-      it ("should EditDataSet")(pending)
-      it ("should EditElement")(pending)
-      it ("should EditElementDefintion")(pending)
-      it ("should EditElementPropertyDefinition")(pending)
-      it ("should EngineCmdResult")(pending)
-      it ("should EngineCommands")(pending)
-      it ("should FindAssociationById")(pending)
-      it ("should FindDataSetById")(pending)
-      it ("should FindDataSetByName")(pending)
-      it ("should FindDownStreamElementsByElementId")(pending)
-      it ("should FindElementById")(pending)
-      it ("should FindElementDefinition")(pending)
-      it ("should FindElementDefinitionById")(pending)
-      it ("should FindElementDefinitionByName")(pending)
-      it ("should FindInboundAssociationsByElementId")(pending)
-      it ("should FindOutboundAssociationsByElementId")(pending)
-      it ("should FindUpStreamElementsByElementId")(pending)
-      it ("should GraphCommandOptions")(pending)
-      it ("should InternalEngineCommand")(pending)
-      it ("should ListAllElementDefinitions")(pending)
-      it ("should ListDataSets")(pending)
-      it ("should RemoveAssociationField")(pending)
-      it ("should RemoveElementField")(pending)
-      it ("should RemoveElementPropertyDefinition")(pending)
-      it ("should RemoveInboundAssociations")(pending)
-      it ("should RemoveOutboundAssociations")(pending)
+      describe("System Space"){
+        it ("should CreateElementDefinition")(pending)
+        it ("should ListAllElementDefinitions")(pending)
+        it ("should EditElementDefintion")(pending)
+        it ("should EditElementPropertyDefinition")(pending)
+        it ("should RemoveElementPropertyDefinition")(pending)
+        it ("should DeleteElementDefintion")(pending)
+        it ("should FindElementDefinition")(pending)
+        it ("should FindElementDefinitionById")(pending)
+        it ("should FindElementDefinitionByName")(pending)
+      }
+
+      describe("User Space"){
+        it ("should CreateElementDefinition")(pending)
+        it ("should ListAllElementDefinitions")(pending)
+        it ("should EditElementDefintion")(pending)
+        it ("should EditElementPropertyDefinition")(pending)
+        it ("should RemoveElementPropertyDefinition")(pending)
+        it ("should DeleteElementDefintion")(pending)
+        it ("should FindElementDefinition")(pending)
+        it ("should FindElementDefinitionById")(pending)
+        it ("should FindElementDefinitionByName")(pending)
+
+        it ("should CreateDataSet")(pending)
+        it ("should ListDataSets")(pending)
+
+        it ("should EditDataSet")(pending)
+        it ("should FindDataSetById")(pending)
+        it ("should FindDataSetByName")(pending)
+      }
+
+      describe("Datasets"){
+        it ("should CreateElementDefinition")(pending)
+        it ("should ListAllElementDefinitions")(pending)
+        it ("should EditElementDefintion")(pending)
+        it ("should EditElementPropertyDefinition")(pending)
+        it ("should RemoveElementPropertyDefinition")(pending)
+        it ("should DeleteElementDefintion")(pending)
+        it ("should FindElementDefinition")(pending)
+        it ("should FindElementDefinitionById")(pending)
+        it ("should FindElementDefinitionByName")(pending)
+
+        it ("should CreateElement")(pending)
+        it ("should DeleteAssociation")(pending)
+        it ("should DeleteElement")(pending)
+
+        it ("should AssociateElements")(pending)
+        it ("should EditAssociation")(pending)
+        it ("should RemoveInboundAssociations")(pending)
+        it ("should RemoveOutboundAssociations")(pending)
+
+        it ("should EditElement")(pending)
+
+        it ("should FindDownStreamElementsByElementId")(pending)
+        it ("should FindElementById")(pending)
+
+        it ("should FindAssociationById")(pending)
+        it ("should FindInboundAssociationsByElementId")(pending)
+        it ("should FindOutboundAssociationsByElementId")(pending)
+        it ("should FindUpStreamElementsByElementId")(pending)
+        it ("should RemoveAssociationField")(pending)
+        it ("should RemoveElementField")(pending)
+      }
     }
   }
+
+  def tm(msg: String):Message = TextMessage(msg)
 
   def createPrintSink(): Sink[Message, Future[Done]] = Sink.foreach {
     case message: TextMessage.Strict => {
@@ -163,4 +179,5 @@ class WebServerSpec extends TestKit(ActorSystem("AkkaHTTPSpike")) with ImplicitS
       println(message.text)
     }
   }
+
 }
