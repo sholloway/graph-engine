f6d3473844ca27e5966d674e3e64d7e633364dd7
Samuel Holloway <sholloway@gmail.com>
2017-09-09 18:23:33 -0500

    Updated ElementAssociationsSpec to leverage the active user.

--- src/test/scala/org/machine/engine/graph/ElementAssociationsSpec.scala
+++ src/test/scala/org/machine/engine/graph/ElementAssociationsSpec.scala
@@ -20,26 +20,38 @@ import org.machine.engine.exceptions._
 import org.machine.engine.graph.nodes._
 
 
-class ElementAssociationsSpec extends FunSpec with Matchers with EasyMockSugar with BeforeAndAfterAll{
+class ElementAssociationsSpec extends FunSpec
+  with Matchers
+  with EasyMockSugar
+  with BeforeAndAfterAll{
   import Neo4JHelper._
   import TestUtils._
-  var engine:Engine = null
-  var systemsDataSetId:String = null
-  var noteElementDefininitionId:String = null
-  var systemElementDefinitionId:String = null
-  var systemId:String = null
-  var noteId:String = null
-  var workerElementDefinitionId:String = null
-  var bizCapElementDefId:String = null
-  var workerId:String = null
-  var erpId:String = null
-  var bizCapId:String = null
+  private var engine:Engine = null
+  private var systemsDataSetId:String = null
+  private var noteElementDefininitionId:String = null
+  private var systemElementDefinitionId:String = null
+  private var systemId:String = null
+  private var noteId:String = null
+  private var workerElementDefinitionId:String = null
+  private var bizCapElementDefId:String = null
+  private var workerId:String = null
+  private var erpId:String = null
+  private var bizCapId:String = null
+  private var activeUserId:String = null
 
   override def beforeAll(){
     engine = Engine.getInstance
     perge
-    systemsDataSetId = engine.createDataSet("System Under Review", "System that need to be reviewed.")
-    noteElementDefininitionId = engine
+    activeUserId = Engine.getInstance
+      .createUser
+      .withFirstName("Bob")
+      .withLastName("Grey")
+      .withEmailAddress("onebadclown@derry-maine.com")
+      .withUserName("pennywise")
+      .withUserPassword("You'll float too...")
+    .end
+    systemsDataSetId = engine.forUser(activeUserId).createDataSet("System Under Review", "System that need to be reviewed.")
+    noteElementDefininitionId = engine.forUser(activeUserId)
       .onDataSet(systemsDataSetId)
       .defineElement("note", "short piece of text")
         .withProperty("title", "String", "The title of the note.")
@@ -47,56 +59,56 @@ class ElementAssociationsSpec extends FunSpec with Matchers with EasyMockSugar w
         .withProperty("body", "String", "The body of the note.")
     .end
 
-    systemElementDefinitionId = engine
+    systemElementDefinitionId = engine.forUser(activeUserId)
       .onDataSet(systemsDataSetId)
       .defineElement("system", "A set of interacting or interdependent components forming an integrated whole.")
         .withProperty("name", "String", "The name of the system.")
         .withProperty("description", "String", "An optional description of the system.")
     .end
 
-    workerElementDefinitionId = engine
+    workerElementDefinitionId = engine.forUser(activeUserId)
       .onDataSet(systemsDataSetId)
       .defineElement("worker", "An employee or contractor who works for a company.")
         .withProperty("first_name", "String", "The primary name of the person.")
         .withProperty("last_name", "String", "The family name of the person.")
     .end
 
-    bizCapElementDefId = engine
+    bizCapElementDefId = engine.forUser(activeUserId)
       .onDataSet(systemsDataSetId)
       .defineElement("business_capability", "The expression or the articulation of the capacity, materials and expertise an organization needs in order to perform core functions.")
       .withProperty("name", "String", "The name of the capability.")
       .withProperty("description", "String", "The description of the capability.")
     .end
 
-    systemId = engine
+    systemId = engine.forUser(activeUserId)
       .onDataSet(systemsDataSetId)
       .provision(systemElementDefinitionId)
         .withField("name", "Publishing System")
         .withField("description", "Publishes stuff.")
     .end
 
-    erpId = engine
+    erpId = engine.forUser(activeUserId)
       .onDataSet(systemsDataSetId)
       .provision(systemElementDefinitionId)
         .withField("name", "ERP System")
         .withField("description", "Manages OTC, Accounts...")
     .end
 
-    noteId = engine
+    noteId = engine.forUser(activeUserId)
       .onDataSet(systemsDataSetId)
       .provision(noteElementDefininitionId)
         .withField("title", "Quick Note")
         .withField("body", "Talk to Chuck about what is being published.")
     .end
 
-    workerId = engine
+    workerId = engine.forUser(activeUserId)
       .onDataSet(systemsDataSetId)
       .provision(workerElementDefinitionId)
       .withField("first_name", "Jon")
       .withField("last_name", "Snow")
     .end
 
-    bizCapId = engine
+    bizCapId = engine.forUser(activeUserId)
       .onDataSet(systemsDataSetId)
       .provision(bizCapElementDefId)
       .withField("name", "Publishing")
@@ -112,7 +124,7 @@ class ElementAssociationsSpec extends FunSpec with Matchers with EasyMockSugar w
     describe("DataSet"){
       describe("Element Associations"){
         it("should associate two elements"){
-          val annotationId = engine
+          val annotationId = engine.forUser(activeUserId)
             .onDataSet(systemsDataSetId)
             .attach(noteId)
             .to(systemId)
@@ -120,7 +132,7 @@ class ElementAssociationsSpec extends FunSpec with Matchers with EasyMockSugar w
             .withField("createdBy", "A. Sterling")
           .end
 
-          val annotation = engine
+          val annotation = engine.forUser(activeUserId)
             .onDataSet(systemsDataSetId)
             .findAssociation(annotationId)
 
@@ -136,14 +148,14 @@ class ElementAssociationsSpec extends FunSpec with Matchers with EasyMockSugar w
         }
 
         it ("should associate without specifying as()"){
-          val annotationId = engine
+          val annotationId = engine.forUser(activeUserId)
             .onDataSet(systemsDataSetId)
             .attach(noteId)
             .to(systemId)
             .withField("createdBy", "A. Sterling")
           .end
 
-          val annotation = engine
+          val annotation = engine.forUser(activeUserId)
             .onDataSet(systemsDataSetId)
             .findAssociation(annotationId)
 
@@ -157,7 +169,7 @@ class ElementAssociationsSpec extends FunSpec with Matchers with EasyMockSugar w
         }
 
         it("should update properties on an association"){
-          val annotationId = engine
+          val annotationId = engine.forUser(activeUserId)
             .onDataSet(systemsDataSetId)
             .attach(noteId)
             .to(systemId)
@@ -165,13 +177,13 @@ class ElementAssociationsSpec extends FunSpec with Matchers with EasyMockSugar w
             .withField("createdBy", "A. Sterling")
           .end
 
-          engine
+          engine.forUser(activeUserId)
             .onDataSet(systemsDataSetId)
             .onAssociation(annotationId)
             .setField("createdBy", "Pam")
           .end
 
-          val association = engine
+          val association = engine.forUser(activeUserId)
             .onDataSet(systemsDataSetId)
             .findAssociation(annotationId)
 
@@ -179,7 +191,7 @@ class ElementAssociationsSpec extends FunSpec with Matchers with EasyMockSugar w
         }
 
         it("should remove an association between two elements"){
-          val annotationId = engine
+          val annotationId = engine.forUser(activeUserId)
             .onDataSet(systemsDataSetId)
             .attach(noteId)
             .to(systemId)
@@ -187,7 +199,7 @@ class ElementAssociationsSpec extends FunSpec with Matchers with EasyMockSugar w
             .withField("createdBy", "Cherl")
           .end
 
-          engine
+          engine.forUser(activeUserId)
             .onDataSet(systemsDataSetId)
             .onAssociation(annotationId)
             .delete
@@ -195,14 +207,14 @@ class ElementAssociationsSpec extends FunSpec with Matchers with EasyMockSugar w
 
           val expectedMsg = "No association with associationId: %s could be found.".format(annotationId)
           the [InternalErrorException] thrownBy{
-            engine
+            engine.forUser(activeUserId)
               .inUserSpace
               .findAssociation(annotationId)
           }should have message expectedMsg
         }
 
         it ("should remove a property on a relationship"){
-          val annotationId = engine
+          val annotationId = engine.forUser(activeUserId)
             .onDataSet(systemsDataSetId)
             .attach(noteId)
             .to(systemId)
@@ -212,37 +224,37 @@ class ElementAssociationsSpec extends FunSpec with Matchers with EasyMockSugar w
             .withField("amazement", "Wow...")
           .end
 
-          engine
+          engine.forUser(activeUserId)
             .inUserSpace
             .findAssociation(annotationId)
             .fields should have size 3
 
-          engine
+          engine.forUser(activeUserId)
             .onDataSet(systemsDataSetId)
             .onAssociation(annotationId)
             .removeField("meta")
           .end
 
-          engine
+          engine.forUser(activeUserId)
             .inUserSpace
             .findAssociation(annotationId)
             .fields should have size 2
 
-          engine
+          engine.forUser(activeUserId)
             .onDataSet(systemsDataSetId)
             .onAssociation(annotationId)
             .removeField("createdBy")
             .removeField("amazement")
           .end
 
-          engine
+          engine.forUser(activeUserId)
             .inUserSpace
             .findAssociation(annotationId)
             .fields should have size 0
         }
 
         it("should find outbound associations of an element"){
-          val annotationId = engine
+          val annotationId = engine.forUser(activeUserId)
             .onDataSet(systemsDataSetId)
             .attach(noteId)
             .to(systemId)
@@ -250,7 +262,7 @@ class ElementAssociationsSpec extends FunSpec with Matchers with EasyMockSugar w
             .withField("createdBy", "J. Snow")
           .end
 
-          val contactId = engine
+          val contactId = engine.forUser(activeUserId)
             .onDataSet(systemsDataSetId)
             .attach(workerId)
             .to(systemId)
@@ -258,7 +270,7 @@ class ElementAssociationsSpec extends FunSpec with Matchers with EasyMockSugar w
             .withField("primaryContact", true)
           .end
 
-          val capId = engine
+          val capId = engine.forUser(activeUserId)
             .onDataSet(systemsDataSetId)
             .attach(systemId)
             .to(bizCapId)
@@ -266,7 +278,7 @@ class ElementAssociationsSpec extends FunSpec with Matchers with EasyMockSugar w
             .withField("identifiedBy", "Jon Snow")
           .end
 
-          val integrationId = engine
+          val integrationId = engine.forUser(activeUserId)
             .onDataSet(systemsDataSetId)
             .attach(systemId)
             .to(erpId)
@@ -274,7 +286,7 @@ class ElementAssociationsSpec extends FunSpec with Matchers with EasyMockSugar w
             .withField("note", "Existing integration shall be replaced next year.")
           .end
 
-          val outBoundAssociations:Seq[Association] = engine
+          val outBoundAssociations:Seq[Association] = engine.forUser(activeUserId)
             .onDataSet(systemsDataSetId)
             .onElement(systemId)
             .findOutboundAssociations()
@@ -301,12 +313,12 @@ class ElementAssociationsSpec extends FunSpec with Matchers with EasyMockSugar w
 
         it("should find inbound associations of an element"){
           //need to clear the existing connections or create a new node.
-          engine
+          engine.forUser(activeUserId)
             .onDataSet(systemsDataSetId)
             .onElement(systemId)
             .removeInboundAssociations
 
-          val annotationId = engine
+          val annotationId = engine.forUser(activeUserId)
             .onDataSet(systemsDataSetId)
             .attach(noteId)
             .to(systemId)
@@ -314,7 +326,7 @@ class ElementAssociationsSpec extends FunSpec with Matchers with EasyMockSugar w
             .withField("createdBy", "J. Snow")
           .end
 
-          val contactId = engine
+          val contactId = engine.forUser(activeUserId)
             .onDataSet(systemsDataSetId)
             .attach(workerId)
             .to(systemId)
@@ -322,7 +334,7 @@ class ElementAssociationsSpec extends FunSpec with Matchers with EasyMockSugar w
             .withField("primaryContact", true)
           .end
 
-          val inboundAssociations:Seq[Association] = engine
+          val inboundAssociations:Seq[Association] = engine.forUser(activeUserId)
             .onDataSet(systemsDataSetId)
             .onElement(systemId)
             .findInboundAssociations()
@@ -345,12 +357,12 @@ class ElementAssociationsSpec extends FunSpec with Matchers with EasyMockSugar w
         }
 
         it("should find upstream (parents) elements"){
-          engine
+          engine.forUser(activeUserId)
             .onDataSet(systemsDataSetId)
             .onElement(systemId)
             .removeInboundAssociations
 
-          val annotationId = engine
+          val annotationId = engine.forUser(activeUserId)
             .onDataSet(systemsDataSetId)
             .attach(noteId)
             .to(systemId)
@@ -358,7 +370,7 @@ class ElementAssociationsSpec extends FunSpec with Matchers with EasyMockSugar w
             .withField("createdBy", "J. Snow")
           .end
 
-          val contactId = engine
+          val contactId = engine.forUser(activeUserId)
             .onDataSet(systemsDataSetId)
             .attach(workerId)
             .to(systemId)
@@ -366,13 +378,12 @@ class ElementAssociationsSpec extends FunSpec with Matchers with EasyMockSugar w
             .withField("primaryContact", true)
           .end
 
-          val upstreamElements:Seq[Element] = engine
+          val upstreamElements:Seq[Element] = engine.forUser(activeUserId)
             .onDataSet(systemsDataSetId)
             .onElement(systemId)
             .findUpStreamElements
 
-          upstreamElements should have length 2
-          println(upstreamElements)
+          upstreamElements should have length 2    
 
           upstreamElements(0) should have(
             'id (noteId)
@@ -384,12 +395,12 @@ class ElementAssociationsSpec extends FunSpec with Matchers with EasyMockSugar w
         }
 
         it("should find downstream (children) elements"){
-          engine
+          engine.forUser(activeUserId)
             .onDataSet(systemsDataSetId)
             .onElement(systemId)
             .removeOutboundAssociations
 
-          val capId = engine
+          val capId = engine.forUser(activeUserId)
             .onDataSet(systemsDataSetId)
             .attach(systemId)
             .to(bizCapId)
@@ -397,7 +408,7 @@ class ElementAssociationsSpec extends FunSpec with Matchers with EasyMockSugar w
             .withField("identifiedBy", "Jon Snow")
           .end
 
-          val integrationId = engine
+          val integrationId = engine.forUser(activeUserId)
             .onDataSet(systemsDataSetId)
             .attach(systemId)
             .to(erpId)
@@ -405,7 +416,7 @@ class ElementAssociationsSpec extends FunSpec with Matchers with EasyMockSugar w
             .withField("note", "Existing integration shall be replaced next year.")
           .end
 
-          val downstreamElements:Seq[Element] = engine
+          val downstreamElements:Seq[Element] = engine.forUser(activeUserId)
             .onDataSet(systemsDataSetId)
             .onElement(systemId)
             .findDownStreamElements
