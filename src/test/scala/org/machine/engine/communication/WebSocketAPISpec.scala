b0cf823a36f267ea5617cce6b4fc087ba1deae1c
Samuel Holloway <sholloway@gmail.com>
2018-08-31 06:08:59 -0500

    Refactored the AsyncAPI tests.

--- src/test/scala/org/machine/engine/communication/WebSocketAPISpec.scala
+++ src/test/scala/org/machine/engine/communication/WebSocketAPISpec.scala
@@ -8,51 +8,56 @@ that make this test suite redundant.
 package org.machine.engine.communication
 import org.scalatest._
 
+import cats.syntax.either._
+import io.circe._
+import io.circe.generic.auto._
+import io.circe.yaml
+import io.circe.yaml.parser
+import java.io.InputStreamReader;
+import scala.io.Source;
+import java.io.File
+
+import com.typesafe.config._
+
 class WebSocketAPISpec extends FunSpecLike
   with Matchers
   with BeforeAndAfterAll{
 
+  val config = ConfigFactory.load()
+  var json:Json = null
   override def beforeAll(){
-  
-  }
+    //Note: The cats library also defines a getClass, hence the "this" inclusion.
+    val inputStream = this.getClass.getClassLoader.getResourceAsStream("asyncapi/commands.yaml")
+    val jsonEither: Either[ParsingFailure, Json] = parser.parse(new InputStreamReader(inputStream))
+    // println(jsonEither)
 
-  override def afterAll(){
-    
+    json = jsonEither
+      .leftMap((error) => { 
+        println(s"An error occured parsing the API spec. ${error}")
+        fail()
+      })
+      .right //This gives us a RightProjection
+      .get //Unwraps the RightProject
   }
 
   describe("Verify Websocket API Specification"){
-    it ("load the file"){
-      import cats.syntax.either._
-      import io.circe._
-      // import io.circe.parser._
-      import io.circe.generic.auto._
-      import io.circe.yaml
-      import io.circe.yaml.parser
-      import java.io.InputStreamReader;
-      import scala.io.Source;
-      import java.io.File
+    it ("should use version 1.2.0 of AsyncAPI spec"){
+      json.hcursor
+        .get[String]("asyncapi")
+        .toOption
+        .get should equal("1.2.0")
+    }
 
-      //Note: The cats library also defines a getClass, hence the "this" inclusion.
-      val inputStream = this.getClass.getClassLoader.getResourceAsStream("asyncapi/commands.yaml")
-      val jsonEither: Either[ParsingFailure, Json] = parser.parse(new InputStreamReader(inputStream))
-      println(jsonEither)
-
-      val json:Json = jsonEither
-        .leftMap((error) => { 
-          println(s"An error occured parsing the API spec. ${error}")
-          fail()
-        })
-        .right //This gives us a RightProjection
-        .get //Unwraps the RightProject
-        
-      val specVersionOption: Option[String] = json.hcursor.get[String]("asyncapi").toOption
-      specVersionOption.get should equal("1.2.0")
-     
-      val apiVersionOption: Option[String] = json.hcursor
+    it ("should identify what version the API is at"){
+      val apiVersion = config.getString("engine.version")
+      json.hcursor
         .downField("info")
-        .get[String]("version").toOption
-      apiVersionOption.get should equal("0.1.0")
-      
+        .get[String]("version")
+        .toOption
+        .get should equal(apiVersion)
+    }
+
+    it ("should declare all the inbound messages the service can receive"){
       val receiveEvents: Vector[Json] = json.hcursor
         .downField("events")
         .downField("receive")
@@ -60,7 +65,9 @@ class WebSocketAPISpec extends FunSpecLike
         .flatMap(_.asArray)
         .getOrElse(Vector.empty)
       receiveEvents.length should equal(1)      
-      
+    }
+
+    it ("should declare all the inbound messages the service can send"){
       val sendEvents: Vector[Json] = json.hcursor
         .downField("events")
         .downField("send")
@@ -68,27 +75,6 @@ class WebSocketAPISpec extends FunSpecLike
         .flatMap(_.asArray)
         .getOrElse(Vector.empty)
       sendEvents.length should equal(1)      
-
-      
-
-      /* Next steps
-      1. Use a before to parse the YAML once.
-      2. Split the above into multiple tests.
-      3. Design the rest of the Layout API
-      4. Evaluate if it's worth trying to do this at run time 
-         to verify the APIs requests and respones. 
-         
-      Note: 
-      If I do use this at runtime, need to only parse the YAML upon 
-      engine start up. Will need to evaluate migrating from Lift to Circe 
-      for JSON duties. No point in having multiple JSON frameworks if 
-      it can be avoided.
-
-      I do feel like the whole AsyncAPI thing is a rabbit hole 
-      that is distracting me from getting the core app working.
-      On the flip side, documenting the API needs to happen
-      and could potentially help simplify building the Cockpit.
-      */
     }
   }
 }
