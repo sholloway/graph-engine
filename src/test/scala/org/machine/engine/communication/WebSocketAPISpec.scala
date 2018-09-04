b4d63d230f9e4bbbaea7b9d46b5e996a452c845a
Samuel Holloway <sholloway@gmail.com>
2018-09-04 05:31:01 -0500

    refactored tests

--- src/test/scala/org/machine/engine/communication/WebSocketAPISpec.scala
+++ src/test/scala/org/machine/engine/communication/WebSocketAPISpec.scala
@@ -41,40 +41,84 @@ class WebSocketAPISpec extends FunSpecLike
   }
 
   describe("Verify Websocket API Specification"){
-    it ("should use version 1.2.0 of AsyncAPI spec"){
-      json.hcursor
-        .get[String]("asyncapi")
-        .toOption
-        .get should equal("1.2.0")
-    }
+    describe("General API"){
+      it ("should use version 1.2.0 of AsyncAPI spec"){
+        json.hcursor
+          .get[String]("asyncapi")
+          .toOption
+          .get should equal("1.2.0")
+      }
 
-    it ("should identify what version the API is at"){
-      val apiVersion = config.getString("engine.version")
-      json.hcursor
-        .downField("info")
-        .get[String]("version")
-        .toOption
-        .get should equal(apiVersion)
-    }
+      it ("should identify what version the API is at"){
+        val apiVersion = config.getString("engine.version")
+        json.hcursor
+          .downField("info")
+          .get[String]("version")
+          .toOption
+          .get should equal(apiVersion)
+      }
 
-    it ("should declare all the inbound messages the service can receive"){
-      val receiveEvents: Vector[Json] = json.hcursor
-        .downField("events")
-        .downField("receive")
-        .focus
-        .flatMap(_.asArray)
-        .getOrElse(Vector.empty)
-      receiveEvents.length should equal(1)      
-    }
+      it ("should declare all the inbound messages the service can receive"){
+        val receiveEvents: Vector[Json] = json.hcursor
+          .downField("events")
+          .downField("receive")
+          .focus
+          .flatMap(_.asArray)
+          .getOrElse(Vector.empty)
+        receiveEvents.length should equal(1)      
+      }
 
-    it ("should declare all the inbound messages the service can send"){
-      val sendEvents: Vector[Json] = json.hcursor
-        .downField("events")
-        .downField("send")
-        .focus
-        .flatMap(_.asArray)
-        .getOrElse(Vector.empty)
-      sendEvents.length should equal(1)      
+      it ("should declare all the inbound messages the service can send"){
+        val sendEvents: Vector[Json] = json.hcursor
+          .downField("events")
+          .downField("send")
+          .focus
+          .flatMap(_.asArray)
+          .getOrElse(Vector.empty)
+        sendEvents.length should equal(1)      
+      }
     }
+    describe("User Facing Messages"){
+     describe("Layout Definitions"){
+       it ("should define listing layout definitions"){
+        val layoutDefsJson: Option[Json] = json.hcursor
+          .downField("components")
+          .downField("messages")
+          .get[Json]("layoutDefinitions")
+          .toOption
+        layoutDefsJson should not be(null)
+        layoutDefsJson.isEmpty should be(false)
+       }
+
+       it ("should define a layout definition"){
+          val layoutDefJson: Option[Json] = json.hcursor
+            .downField("components")
+            .downField("schemas")
+            .get[Json]("layoutDefinition")
+            .toOption
+          layoutDefJson should not be(null)
+          layoutDefJson.isEmpty should be(false) 
+          expectSimpleObjectStrProperty(layoutDefJson, "id", "string")
+          expectSimpleObjectStrProperty(layoutDefJson, "name", "string")
+          expectSimpleObjectStrProperty(layoutDefJson, "description", "string")
+          expectSimpleObjectStrProperty(layoutDefJson, "creation_time", "string")
+          expectSimpleObjectStrProperty(layoutDefJson, "last_modified_time", "string")
+       } 
+     }
+    }
+  }
+
+  def expectSimpleObjectStrProperty(jsonOption: Option[Json], 
+    expectedPropertyName: String, expectedPropertyType: String):Unit = {
+    jsonOption should not be(null)
+    jsonOption.isEmpty should be(false)
+    val propertyOption = jsonOption.get.hcursor
+      .downField("properties")
+      .downField(expectedPropertyName)
+      .getOrElse[String]("type")(null) //A getOrElse might be better here.
+      .toOption
+
+    propertyOption.isEmpty should be(false)
+    propertyOption.get should be(expectedPropertyType)
   }
 }
